a7418c132a9262d7b7c3e7ee36d96cff7b2b7454
robertoCastro <bkastro@gmail.com>
2024-08-10 23:47:59 -0300

    Criação da pasta ContactForm com seus respectivos arquivos jsx e css

---
+++ dnc-arquitetura/src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,12 @@
+import './ContactForm.css';
+
+function ContactForm() {
+  return (
+    <div className="contact-form d-flex fd-column al-center">
+      <h2>We love meeting new people and helping them.</h2>
+      <form action="">Work in Progress</form>
+    </div>
+  )
+}
+
+export default ContactForm;
