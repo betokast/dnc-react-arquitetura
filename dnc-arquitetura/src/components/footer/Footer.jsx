1ae912c904af9b2dab1308b51376723af6e80485
robertoCastro <bkastro@gmail.com>
2024-08-10 23:42:45 -0300

    Ajustes no componente para a responsividade

--- dnc-arquitetura/src/components/footer/Footer.jsx
+++ dnc-arquitetura/src/components/footer/Footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
   return (
     <footer>
       <div className='container'>
-        <div className="d-flex jc-space-between">
+        <div className="d-flex jc-space-between mobile-fd-column">
           <div className="footer-logo-col">
             <Link to="/"><img src={Logo} /></Link>
             <p className="grey-color-1">A escola que prepara você para as profissões em alta no mercado de trabalho.</p>
@@ -36,7 +36,7 @@ function Footer() {
               </a>
             </div>
           </div>
-          <div className="d-flex">
+          <div className="d-flex mobile-fd-column">
               <div className="footer-col">
                 <h3>Pages</h3>
                 <ul>
