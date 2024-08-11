7a44ac6a692ac2e4b4328b3da7c26f93753e9f02
robertoCastro <bkastro@gmail.com>
2024-08-10 23:45:59 -0300

    Inclusão do componente ProjectList

--- dnc-arquitetura/src/pages/projects/Projects.jsx
+++ dnc-arquitetura/src/pages/projects/Projects.jsx
@@ -1,13 +1,16 @@
 import Banner from "../../components/banner/Banner";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/Header/Header";
+import ProjectsList from "../../components/ProjectList/ProjectsList";
 
 function Projects() {
   return(
     <>
       <Header />
-      <Banner title="Projects" image='projects.svg' />
-      <p>Subtitle projects</p>
+        <Banner title="Projects" image='projects.svg' />
+        <div className="container">
+          <ProjectsList />
+        </div>
       <Footer />
     </>
   )
