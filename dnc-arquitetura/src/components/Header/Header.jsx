510b2f261ae43aeac384974e8833d72146fd615a
robertoCastro <bkastro@gmail.com>
2024-08-15 21:24:01 -0300

    Implementação do hook useState para chamar ou não (a depender do estado) a função que irá controlar o menu

--- dnc-arquitetura/src/components/Header/Header.jsx
+++ dnc-arquitetura/src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+// ===== IMPORTS
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 // ===== ASSETS
@@ -6,13 +8,29 @@ import Logo from '../../assets/dnc-logo.svg';
 // ===== STYLES
 import './Header.css'
 
+// ===== COMPONENTS
+import Button from "../Button/Button";
+
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <header>
       <div className="container">
         <div className="al-center d-flex jc-space-between">
           <Link to="/"><img src={Logo} /></Link>
-          <nav>
+          <div className="mobile-menu">
+            <Button buttonStyle="secondary" onClick={toggleMenu}>
+              Menu
+            </Button>
+          </div>
+          <nav className={`${isOpen ? 'open' : ''}`}>
+            <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={toggleMenu}>
+              X
+            </Button>
             <ul className="d-flex">
               <li><NavLink to="/">Home</NavLink></li>
               <li><NavLink to="/about">About</NavLink></li>
