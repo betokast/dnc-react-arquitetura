27615d140eb40cc20be9e061296de6f9de95280a
robertoCastro <bkastro@gmail.com>
2024-08-26 20:51:58 -0300

    Implementação de função utilitária para mover as páginas pro início sempre que houver troca de rotas

--- dnc-arquitetura/src/App.jsx
+++ dnc-arquitetura/src/App.jsx
@@ -1,15 +1,19 @@
 // ==== ROUTES ====
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // ==== PAGES ====
-import Home from './pages/home/Home'
-import About from './pages/about/About'
-import Projects from './pages/projects/Projects'
-import Contact from './pages/contact/Contact'
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Projects from "./pages/projects/Projects";
+import Contact from "./pages/contact/Contact";
+
+// ==== UTILS ====
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/*Função sendo chamada para atualizar a montagem */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
@@ -17,7 +21,7 @@ function App() {
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
