b86666276333c082d1b990decbea012e0bd5d4ea
robertoCastro <bkastro@gmail.com>
2024-09-06 10:30:15 -0300

    Implementação de servidor proxy para contornar bloqueio de CORS

--- dnc-arquitetura/vite.config.js
+++ dnc-arquitetura/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: { //Utilização do servidor proxy para contornar bloqueios de CORS nas requisições
+      '/files': {
+        target: 'https://react.dnc.group/files',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/files/,''),
+      }
+    }
+  }
 })
