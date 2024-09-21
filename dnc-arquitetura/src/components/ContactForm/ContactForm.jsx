d0bad462920b8438dd6ed58e1252f7864ef818a4
robertoCastro <bkastro@gmail.com>
2024-09-20 21:30:36 -0300

    Finalização da validação com verificação para sucesso da requisição POST

--- dnc-arquitetura/src/components/ContactForm/ContactForm.jsx
+++ dnc-arquitetura/src/components/ContactForm/ContactForm.jsx
@@ -3,24 +3,43 @@ import Button from '../Button/Button';
 import './ContactForm.css';
 
 function ContactForm() {
-
-    const [ isFormValid, setIsFormValid ] = useState(false);
     const [ formData, setFormData ] = useState({
         name: '',
         email: '',
         message: ''
     })
+    const [ isFormValid, setIsFormValid ] = useState(false);
+    const [ formSubmitLoading, setFormSubmitLoading ] = useState(false);
+    const [ formSubmitted, setFormSubmitted ] = useState(false);
+
     //Função de submit:
     const handleSubmit = async (event) => {
-        event.preventDefault();
+        event.preventDefault(); //Prevenção do disparo padrão ao clicar no submit
         if(isFormValid) {
-            null;
+            setFormSubmitLoading(true);
+            try {
+                const response = await fetch('https://api.web3forms.com/submit', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({...formData, access_key: "95407290-0247-431d-a36d-da1a07411b01"})
+                })
+
+                if(response.ok) {
+                    setFormSubmitted(true);
+                } else {
+                    alert('Erro ao enviar!');
+                }
+            }catch(event) {
+                alert(`Error: ${event}`)
+            }finally {
+                setFormSubmitLoading(false);
+            }
         }
     }
 
     useEffect(() => {
         const isValidEmail = (email) => {
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
             return emailRegex.test(email)
         }
 
@@ -43,7 +62,7 @@ function ContactForm() {
   return (
     <div className="contact-form d-flex fd-column al-center">
       <h2>We love meeting new people and helping them.</h2>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <div className="d-flex form-group">
             <input
                 className='form-input'
@@ -75,7 +94,8 @@ function ContactForm() {
             />
         </div>
         <div className='al-center d-flex jc-end form-group'>
-            <Button type="submit" buttonStyle="secondary" disabled={!isFormValid}>
+            {formSubmitted && <p className='text-primary'>Formulário enviado com sucesso</p>}
+            <Button type="submit" buttonStyle="secondary" disabled={ !isFormValid || formSubmitLoading}>
                 Enviar
             </Button>
         </div>
